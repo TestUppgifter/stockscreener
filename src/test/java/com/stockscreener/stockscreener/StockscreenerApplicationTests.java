36787d1135a0ee626826bf5fc200ad21f32a186e
TestUppgifter <k2376@pm.me>
2025-02-28 13:19:54 +0100

    Setup start

---
+++ src/test/java/com/stockscreener/stockscreener/StockscreenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockscreener.stockscreener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockscreenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
